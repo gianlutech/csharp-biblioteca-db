283f86b66016f68b40ec59c701fd0747a1483383
gianlutech <gianluca.ingrao95@gmail.com>
2022-05-25 18:15:25 +0200

    Aggiungere i file di progetto.

---
+++ Bibliloteca.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca_db
+{
+    public class Biblioteca
+    {
+
+        public string Nome { get; set; }
+        public List<Scaffale> ScaffaliBiblioteca { get; set; }
+
+
+        public Biblioteca(string Nome)
+        {
+            this.Nome = Nome;
+            this.ScaffaliBiblioteca = new List<Scaffale>();
+
+        }
+
+        public void AggiungiScaffale(string nomescaffale)
+
+        {
+            Scaffale nuovo = new Scaffale(nomescaffale);
+            this.ScaffaliBiblioteca.Add(nuovo);
+
+            DB.scaffaleAdd(nomescaffale);    //aggiunge le righe dello scaffale nel db        
+
+        }
+
+        public int GestisciOperazioneBiblioteca(int iCodiceOperazione)
+        {
+            List<Documento> lResult;
+            string sAppo;
+            switch (iCodiceOperazione)
+            {
+                case 1:
+                    Console.WriteLine("inserisci autore");
+                    sAppo = Console.ReadLine();
+                    lResult = SearchByAutore(sAppo);
+                    if (lResult == null)
+                        return 1;   //da implementare uscita da inserimento autore
+                    else
+                        StampaListaDocumenti(lResult);
+                    break;
+
+            }
+            return 0;
+        }
+
+
+        public void StampaListaDocumenti(List<Documento> lListDoc)
+        {
+            // da implementare  
+        }
+
+        public List<Documento> SearchByCodice(string Codice)
+        {
+            Console.WriteLine("Metodo da implementare");
+            return null;
+        }
+
+        public List<Documento> SearchByTitolo(string Titolo)
+        {
+            Console.WriteLine("Metodo da implementare");
+            return null;
+
+        }
+
+        public List<Documento> SearchByAutore(string Titolo)
+        {
+            Console.WriteLine("Metodo da implementare");
+
+            // connetti al db
+            // fare una query, quindi select titolo.scaffale, stato , tipo  from 
+            // documenti, autori_documenti,  inner join
+            // stampa 
+
+            return null;
+
+        }
+
+        public List<Prestito> SearchPrestiti(string Numero)
+        {
+            Console.WriteLine("Metodo da implementare");
+            return null;
+        }
+
+        public List<Prestito> SearchPrestiti(string Nome, string Cognome)
+        {
+            Console.WriteLine("Metodo da implementare");
+            return null; ;
+        }
+    }
+}
---
+++ db.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace csharp_biblioteca_db
+{
+    internal class db
+    {
+        private static string stringaDiConnessione = "Data Source=localhost;Initial Catalog=biblioteca;Integrated Security=True;Pooling=False";
+        private static SqlConnection Connect()
+        {
+            SqlConnection conn = new SqlConnection(stringaDiConnessione);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            return conn;
+        }
+        //Il libro inserisce sia in Documenti che in Libri
+        internal static void libroAdd(Libro libro)
+        {
+            var conn = Connect();
+            if (conn == null)
+                throw new Exception("Unable to connect to the dabatase");
+            //Come posso stampare il titolo del libro che ha codice 10001?
+            //select Titolo from Libri, Documenti where Libri.codice=10001;
+            //ma il codice 10001 è il codice di un libro o di un documento oppure 
+            //il documento con codice 10001 rappresenta il libro con codice 10001???
+            //Manca la relazione libri <=> documenti! e anche dvd <=> documenti!!!
+            //insert in documenti
+            var cmd = String.Format("insert into Libri (tutti i campi) values ('{0}', {1}, ...)", tutte le variabili);
+            using (SqlCommand insert = new SqlCommand(cmd, conn))
+            {
+                try
+                {
+                    var numrows = insert.ExecuteNonQuery();
+                    return numrows;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return 0;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+        internal static int scaffaleAdd(string nuovo)
+        {
+            var conn = Connect();
+            if (conn == null)
+                throw new Exception("Unable to connect to the dabatase");
+            var cmd = String.Format("insert into Scaffale (Scaffale) values ('{0}')", nuovo);
+            using (SqlCommand insert = new SqlCommand(cmd, conn))
+            {
+                try
+                {
+                    var numrows = insert.ExecuteNonQuery();
+                    return numrows;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return 0;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+        internal static List<string> scaffaliGet()
+        {
+            List<string> ls = new List<string>();
+            var conn = Connect();
+            if (conn == null)
+                throw new Exception("Unable to connect to the dabatase");
+            var cmd = String.Format("select Scaffale from Scaffale");  //Li prendo tutti
+            using (SqlCommand select = new SqlCommand(cmd, conn))
+            {
+                using (SqlDataReader reader = select.ExecuteReader())
+                {
+                    //Console.WriteLine(reader.FieldCount);
+                    while (reader.Read())
+                    {
+                        ls.Add(reader.GetString(0));
+                    }
+                }
+            }
+            conn.Close();
+            return ls;
+        }
+        //nel caso ci siano più attributi, allora potete utilizzare le tuple
+        internal static List<Tuple<int, string, string, string, string, string>> documentiGet()
+        {
+            var ld = new List<Tuple<int, string, string, string, string, string>>();
+            var conn = Connect();
+            if (conn == null)
+                throw new Exception("Unable to connect to the dabatase");
+            var cmd = String.Format("select codice, Titolo, Settore, Stato, Tipo, Scaffale from Documenti");  //Li prendo tutti
+            using (SqlCommand select = new SqlCommand(cmd, conn))
+            {
+                using (SqlDataReader reader = select.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var data = new Tuple<Int32, string, string, string, string, string>(
+                            reader.GetInt32(0),
+                            reader.GetString(1),
+                            reader.GetString(2),
+                            reader.GetString(3),
+                            reader.GetString(4),
+                            reader.GetString(5));
+                        ld.Add(data);
+                    }
+                }
+            }
+            conn.Close();
+            return ld;
+        }
+    }
+}
